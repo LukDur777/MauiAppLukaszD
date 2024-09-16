90c51fcdb6ff39568fc273ef31b730d31ab5eece
LUK DUR <durawalukasz@wp.pl>
2024-09-16 10:05:27 +0200

    dzialajace bez radiobuttona

--- MauiAppLukaszD/MainPage.xaml.cs
+++ MauiAppLukaszD/MainPage.xaml.cs
@@ -17,6 +17,26 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+        private void OnCounterClicked1(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int diceRoll = r.Next(1, 5);
+
+            RollValueLabel.Text = "Wylosowany rzut: " + diceRoll.ToString();
+
+            SemanticScreenReader.Announce(CounterBtn1.Text);
+        }
+        private void OnCounterClicked2(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int diceRoll = r.Next(1, 13);
+
+            RollValueLabel.Text = "Wylosowany rzut: " + diceRoll.ToString();
+
+            SemanticScreenReader.Announce(CounterBtn2.Text);
+        }
     }
 
 }

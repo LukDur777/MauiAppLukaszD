880364673b3df155b39f689b85bd183c03485f0c
LUK DUR <durawalukasz@wp.pl>
2024-09-16 09:31:54 +0200

    Dodaj pliki projektów.

---
+++ MauiAppLukaszD/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppLukaszD
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppLukaszD/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiAppLukaszD
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int diceRoll = r.Next(1, 7);
+
+            RollValueLabel.Text = "Wylosowany rzut: " + diceRoll.ToString();
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
